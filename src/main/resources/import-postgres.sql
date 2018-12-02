e36dc8bb51e75d936c448453d69861533e0447a1
mikbac <mbachorz1997@gmail.com>
2018-12-02 23:54:22 +0100

    add sql

---
+++ src/main/resources/import-postgres.sql
@@ -0,0 +1,21 @@
+INSERT INTO univeristy_table(name) VALUES('Bratislava');
+
+
+
+
+
+--INSERT INTO faculty_table(name, universityId) VALUES('Bratislava', 1);
+
+
+
+
+
+
+
+--INSERT INTO teacher_table(name, surname, facultyId) VALUES('Bratislava', 1);
+
+
+
+
+
+--INSERT INTO vote_table(attractiveness, competency, difficulty, friendship, system, classes, "comment") VALUES('Bratislava', 432000);
